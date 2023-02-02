c8a56be42721e70da59ea61d427345b9005c1b66
Beltranax <beltranangel7293@gmail.com>
2023-02-02 21:14:45 +0000

    Lab Completed

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -9,7 +9,31 @@ public class MaxMinusMin {
      * @param nums an List<Integer> containing varying nums.
      * @return the max value of nums minus the min value of nums.
      */
+
+     
     public int difference(List<Integer> nums){
-        return 0;
+        int min = nums.get(0);
+        int max = nums.get(0);
+
+        int length = nums.size();
+
+        for (int i = 1; i < length; i++)
+        {
+            if(nums.get(i) < min)
+            {
+                min = nums.get(i);
+            }
+        }
+
+        for (int i = 1; i < length; i++)
+        {
+            if(nums.get(i) > max)
+            {
+                max = nums.get(i);
+            }
+        }
+
+        return max - min;
+
     }
 }
